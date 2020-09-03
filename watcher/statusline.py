0ba3b8c3e93aacdb8b8f59a7d1e620e79cb97c2d
Kovid Goyal <kovid@kovidgoyal.net>
2020-09-03 20:59:56 +0530

    Add errors from YouCompleteMe

--- watcher/statusline.py
+++ watcher/statusline.py
@@ -218,15 +218,13 @@ function g:StatusLine_get_data(winnr)
     let name = bufname(cbufnr)
     let file_directory = name != '' ? fnamemodify(name, ':~:.:h') : ''
     let file_name = name != '' ? fnamemodify(name, ':~:.:t') : ''
-    try
-        if g:SyntasticLoclist.current().hasErrorsOrWarningsToDisplay() > 0
-                let ll = g:SyntasticLoclist.current()
-                let warnings = len(ll.warnings())
-                let errors = len(ll.errors())
-        endif
-    catch /.*/
-        echo "Caught error: " . v:exception
-    endtry
+    if g:SyntasticLoclist.current().hasErrorsOrWarningsToDisplay() > 0
+            let ll = g:SyntasticLoclist.current()
+            let warnings += len(ll.warnings())
+            let errors += len(ll.errors())
+    endif
+    let errors += youcompleteme#GetErrorCount()
+    let warnings += youcompleteme#GetWarningCount()
     let ans = {'mode':m, 'bufname':name, 'file_directory':file_directory, 'file_name':file_name, \
         'readonly':getbufvar(cbufnr, "&readonly"), 'modified':getbufvar(cbufnr, "&modified"), \
         'buftype':getbufvar(cbufnr, "&buftype"), 'fileformat':getbufvar(cbufnr, '&fileformat'), \
